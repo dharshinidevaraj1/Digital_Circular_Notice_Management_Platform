4704616dd4c5b9a052b7647a91abed25f13e1b4a
Dharshini <dharshinidevaraj1@gmail.com>
2026-02-11 15:45:07 +0530

    made changes

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+app.secret_key = "hackathon_secret"
+
+# ---------------- MYSQL CONFIG ----------------
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'dharshini20'
+app.config['MYSQL_DB'] = 'digital_circular'
+
+mysql = MySQL(app)
+
+@app.route('/admin')
+def admin():
+    if session.get('role') != 'admin':
+        return redirect('/')
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM circulars ORDER BY created_at DESC")
+    circulars = cur.fetchall()
+    cur.close()
+
+    return render_template('admin.html', circulars=circulars)
